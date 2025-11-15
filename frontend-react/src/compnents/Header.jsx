352f8fd871245a4ac98546d24d4789eb33b9d9d0
Haroon Rasheed <kharoon274@gmail.com>
2025-11-15 10:55:12 +0500

    logout funtionality

--- frontend-react/src/compnents/Header.jsx
+++ frontend-react/src/compnents/Header.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "./Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
 
 const Header = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setIsLoggedIn(false);
+    const navigate = useNavigate("/login");
+  };
   return (
     <>
       <nav className="navbar container pt-3 pb-3 align-items-start">
@@ -11,9 +20,23 @@ const Header = () => {
           Stock Prediction Portal
         </Link>
         <div>
-          <Button text="Login" class="btn-outline-info" url="/login"></Button>
-          &nbsp;
-          <Button text="Register" class="btn-info" url="/register"></Button>
+          {isLoggedIn ? (
+            // <span className="text-light">Logout</span>
+
+            <button class="btn btn-danger" onClick={handleLogout}>
+              Logut
+            </button>
+          ) : (
+            <>
+              <Button
+                text="Login"
+                class="btn-outline-info"
+                url="/login"
+              ></Button>
+              &nbsp;
+              <Button text="Register" class="btn-info" url="/register"></Button>
+            </>
+          )}
         </div>
       </nav>
     </>
--- frontend-react/src/compnents/Login.jsx
+++ frontend-react/src/compnents/Login.jsx
@@ -1,30 +1,95 @@
-import React from "react";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // loading state after clicking Login button
+  const [loading, setLoading] = useState(false);
+  // state for storing the errors
+  const [error, setError] = useState("");
+
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  // use navigte
+
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // user data object and send it with an api request
+    const userData = {
+      username,
+      password,
+    };
+
+    // axios code
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/v1/token/",
+        userData
+      );
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      setIsLoggedIn(true);
+      navigate("/");
+
+      setError({});
+    } catch (error) {
+      setError("Invalid Credentials");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-6 bg-light-dark p-5 rounded">
-          <h3 className="text-light text-center mb-4">Login</h3>
-          <form action="">
-            <input
-              type="text"
-              name=""
-              id=""
-              className="form-control mb-3"
-              placeholder="Enter username"
-            />
+          <h3 className="text-light text-center mb-4">Login to our Portal</h3>
+          <form action="" onSubmit={onSubmitHandler}>
+            <div className="mb-3">
+              <input
+                type="text"
+                name=""
+                className="form-control "
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
 
-            <input
-              type="password"
-              name=""
-              id=""
-              className="form-control mb-4 "
-              placeholder="Set Password"
-            />
-            <button type="submit" className="btn btn-info d-block mx-auto">
-              Login
-            </button>
+            <div className="mb-4">
+              <input
+                type="password"
+                name=""
+                className="form-control  "
+                placeholder="Set Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {error && <div className="text-danger">{error}</div>}
+            {loading ? (
+              <button
+                type="submit"
+                className="btn btn-info d-block mx-auto"
+                disabled
+              >
+                <FontAwesomeIcon icon={faSpinner} spin></FontAwesomeIcon>
+                Loging in
+              </button>
+            ) : (
+              <button type="submit" className="btn btn-info d-block mx-auto">
+                Login
+              </button>
+            )}
           </form>
         </div>
       </div>
